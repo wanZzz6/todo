ceecbf64b4f0d44254ee1fc57dcf74fd54c27fc2
ComeMorning <1343837706@qq.com>
2019-06-18 14:39:56 +0800

    添加待办事项

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+lst = [
+    {'待办事项': '吃饭', '已完成': False},
+    {'待办事项': '逛街', '已完成': False},
+]
 
 def home(request):
-    return render(request, 'todolist/home.html')
+    global lst
+
+    print(request.method)
+    if request.method == "POST":
+        content = request.POST.get('待办事项')
+        print(content)
+        if content.strip() == '':
+            return render(request, 'todolist/home.html', {'清单': lst})
+        else:
+            lst.append({'待办事项': content , '已完成': False})
+            return render(request, 'todolist/home.html', {'清单': lst})
+    else:
+        return render(request, 'todolist/home.html', {'清单': lst})
+
 
 
 def edit(request):
